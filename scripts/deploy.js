a26ea66954313a03fa721fab1c625992bd2ef46c
David  Uzochukwu <daviduzochukwu@Davids-MacBook-Pro.local>
2021-10-14 10:40:20 +0100

    deployment script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,30 +1,18 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// When running the script with `npx hardhat run <script>` you'll find the Hardhat
-// Runtime Environment's members available in the global scope.
+
 const hre = require("hardhat");
-
 async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
+  const TOKENVESTING = await hre.ethers.getContractFactory("MeshaToken");
+  const token = await TOKENVESTING.deploy();
 
-  // We get the contract to deploy
-  const Greeter = await hre.ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  await token.deployed();
 
-  await greeter.deployed();
-
-  console.log("Greeter deployed to:", greeter.address);
+  console.log("TokenVesting deployed address:", token.address);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
 });
+
+
+// deployed contract address on rinkeby 0xAC6b75140F7C8b7B70148e44829214Da8D64e686
